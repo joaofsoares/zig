696196ee62c2286d3e78b357fbd06d73117b67e4
joaofsoares <joaofsoares@yahoo.com>
2025-01-24 19:34:23 +0000

    improvement user input

--- learn/src/input/user_input.zig
+++ learn/src/input/user_input.zig
@@ -4,19 +4,10 @@ pub fn getUserInput(allocator: std.mem.Allocator) ![]const u8 {
     const std_in = std.io.getStdIn().reader();
     const std_out = std.io.getStdOut().writer();
 
-    var buffer_input: [50]u8 = undefined;
-
     try std_out.print("Enter your name: ", .{});
 
-    _ = try std_in.readUntilDelimiter(buffer_input[0..], '\n');
+    var buffer_input: [50]u8 = undefined;
+    const input = try std_in.readUntilDelimiter(buffer_input[0..], '\n');
 
-    var cnt: usize = 0;
-
-    for (buffer_input) |character| {
-        if (std.ascii.isAlphabetic(character)) {
-            cnt += 1;
-        }
-    }
-
-    return try allocator.dupe(u8, buffer_input[0..cnt]);
+    return try allocator.dupe(u8, input);
 }
