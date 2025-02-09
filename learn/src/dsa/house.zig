520dacfbec32a5766ecc7c3987c0ebd5d5497786
joaofsoares <joaofsoares@yahoo.com>
2025-02-09 18:33:26 +0000

    house

---
+++ learn/src/dsa/house.zig
@@ -0,0 +1,252 @@
+const std = @import("std");
+const testing = std.testing;
+
+const str =
+    \\This is the house that Jack built.
+    \\This is the malt that lay in the house that Jack built.
+    \\This is the rat that ate the malt that lay in the house that Jack built.
+    \\This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    \\This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    \\This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    \\This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    \\This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    \\This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    \\This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    \\This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    \\This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+;
+
+pub fn recite(buffer: []u8, start_verse: u32, end_verse: u32) []const u8 {
+    var cnt: usize = 1;
+    var start_idx: usize = 0;
+    var end_idx: usize = 0;
+    var standard_start_idx: usize = 0;
+
+    while (cnt <= end_verse) : (cnt += 1) {
+        if (cnt < start_verse) {
+            start_idx = std.mem.indexOfPos(u8, str, start_idx + 1, "\n").?;
+        } else if (cnt == start_verse) {
+            standard_start_idx = if (str[start_idx] == '\n') (start_idx + 1) else start_idx;
+            end_idx = std.mem.indexOfPos(u8, str, start_idx + 1, "\n") orelse str.len;
+        } else if (cnt <= end_verse) {
+            start_idx = std.mem.indexOfPos(u8, str, end_idx, "\n").?;
+            end_idx = std.mem.indexOfPos(u8, str, start_idx + 1, "\n") orelse str.len;
+        }
+    }
+
+    const result = str[standard_start_idx..end_idx];
+    @memcpy(buffer[0..result.len], result);
+
+    return buffer[0..result.len];
+}
+
+test "verse one - the house that jack built" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 1, 1);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse two - the malt that lay" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 2, 2);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse three - the rat that ate" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 3, 3);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse four - the cat that killed" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 4, 4);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse five - the dog that worried" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 5, 5);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse six - the cow with the crumpled horn" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 6, 6);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse seven - the maiden all forlorn" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 7, 7);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse eight - the man all tattered and torn" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 8, 8);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse nine - the priest all shaven and shorn" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 9, 9);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse 10 - the rooster that crowed in the morn" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 10, 10);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse 11 - the farmer sowing his corn" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 11, 11);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "verse 12 - the horse and the hound and the horn" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 12, 12);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "multiple verses" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 4, 8);
+
+    try testing.expectEqualStrings(expected, actual);
+}
+
+test "full rhyme" {
+    const buffer_size = 4000;
+
+    var buffer: [buffer_size]u8 = undefined;
+
+    const expected: []const u8 =
+        \\This is the house that Jack built.
+        \\This is the malt that lay in the house that Jack built.
+        \\This is the rat that ate the malt that lay in the house that Jack built.
+        \\This is the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+        \\This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.
+    ;
+
+    const actual = recite(&buffer, 1, 12);
+
+    try testing.expectEqualStrings(expected, actual);
+}
