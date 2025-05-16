d5887b09e3b28d75e15778801396564262ebac98
joaofsoares <joaofsoares@yahoo.com>
2025-05-17 00:18:40 +0100

    better solution for reverse string

--- learn/src/dsa/reverse_string.zig
+++ learn/src/dsa/reverse_string.zig
@@ -2,21 +2,8 @@ const std = @import("std");
 const testing = std.testing;
 
 pub fn reverse(buffer: []u8, s: []const u8) []u8 {
-    if (s.len == 0) {
-        return "";
-    }
-
-    var s_size: usize = s.len - 1;
-    var idx: usize = 0;
-
-    while (s_size >= 0) : (idx += 1) {
-        buffer[idx] = s[s_size];
-
-        if (s_size == 0) {
-            break;
-        }
-
-        s_size -= 1;
+    for (s, 1..) |c, i| {
+        buffer[s.len - i] = c;
     }
 
     return buffer;
