b88bf30d4754b667f2b30e57fcc042b73191034e
João Felipe Soares <joaofsoares@yahoo.com>
2025-07-16 16:55:45 +0100

    named loops

---
+++ learn/src/misc/named_loops.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn main() !void {
+    one: for (0..10) |i| {
+        if (i == 2) {
+            std.debug.print("break loop here\n", .{});
+            break :one;
+        }
+    }
+
+    std.debug.print("after loop break\n", .{});
+}
