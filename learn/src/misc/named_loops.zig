c9db7f4dfa63cc2c71a0f0c8ee064183d6aea9b7
João Felipe Soares <joaofsoares@yahoo.com>
2025-07-16 17:02:44 +0100

    Update named_loops.zig

--- learn/src/misc/named_loops.zig
+++ learn/src/misc/named_loops.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub fn main() !void {
     one: for (0..10) |i| {
+        std.debug.print("i = {d}\n", .{i});
         if (i == 2) {
             std.debug.print("break loop here\n", .{});
             break :one;
